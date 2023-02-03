b800180ab4c7745026cf5a1c629af43b5cc83fca
levinzhang1981 <levinzhang1981@gmail.com>
2023-02-03 20:22:48 +0800

    The first demo

--- java-concurrent-practice/src/main/java/com/levin/concurrent/practice/ThreadSafeTest.java
+++ java-concurrent-practice/src/main/java/com/levin/concurrent/practice/ThreadSafeTest.java
@@ -5,16 +5,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ThreadSafeTest {
-    public static class UnsafeSequence{
+    public static class UnsafeSequence {
         private int value;
-        public int getNext(){
+
+        public int getNext() {
             return value++;
         }
     }
 
-    public static class SafeSequence{
+    public static class SafeSequence {
         private int value;
-        public synchronized int getNext(){
+
+        public synchronized int getNext() {
             return value++;
         }
     }
@@ -25,15 +27,15 @@ public class ThreadSafeTest {
         UnsafeSequence unsafeSequence = new UnsafeSequence();
         SafeSequence safeSequence = new SafeSequence();
         CountDownLatch countDownLatch = new CountDownLatch(2);
-        executor.submit(()->{
-            for(int i=0;i<100000;i++){
+        executor.submit(() -> {
+            for (int i = 0; i < 100000; i++) {
                 safeSequence.getNext();
                 unsafeSequence.getNext();
             }
             countDownLatch.countDown();
         });
-        executor.submit(()->{
-            for(int i=0;i<100000;i++){
+        executor.submit(() -> {
+            for (int i = 0; i < 100000; i++) {
                 safeSequence.getNext();
                 unsafeSequence.getNext();
             }
@@ -42,8 +44,8 @@ public class ThreadSafeTest {
         countDownLatch.await();
         executor.shutdown();
         System.out.println(String.format("The Thread Safe sequence value should be 200000," +
-                " the actual value is:[%d]",safeSequence.value));
+                " the actual value is:[%d]", safeSequence.value));
         System.out.println(String.format("The Not Thread Safe sequence value is unknown," +
-                " the actual value is:[%d]",unsafeSequence.value));
+                " the actual value is:[%d]", unsafeSequence.value));
     }
 }
